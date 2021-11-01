ed1fc2e53adcea5e119ef2a04922a4f402867011
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-11-01 17:04:27 +0300

    add

---
+++ src/components/Profile/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Profile =(props) => {
+    
+        return (
+            <div className="">
+                
+            </div>
+        )
+    
+}
+export default Profile;
