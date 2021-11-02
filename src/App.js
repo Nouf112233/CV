0573c8afd750c32fa753e9266f2ecb29760da418
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-11-02 08:40:41 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "./App.css";
 
 function App() {
   // eslint-disable-next-line
-  const [cv, setCv] = useState({profile:{imge:"..../img/cv.jpg",name:"Nouf Alateeq", carrer:" webSit Developer " ,cuntry:"Saudi Arabia",email:'nouf.ateeq@gmail.com',github:'https://github.com/Nouf112233'},
+  const [cv, setCv] = useState({profile:{imge:`.../img/cv.jpg`,name:"Nouf Alateeq", carrer:" webSit Developer " ,cuntry:"Saudi Arabia",email:'nouf.ateeq@gmail.com',github:'https://github.com/Nouf112233'},
    about:"A trainee at Tuwaiq camp 1000 for programming in the javascript track I love programming and strive for self-development and I made this CV with react",
   skills:["Html","Css","Javascript","Bootstrap","JQuary","Dom","React"],
   education:["Bachelor of Information Technology","Business management diploma","General Education Diploma","Javascript Path Programming Camp"]
